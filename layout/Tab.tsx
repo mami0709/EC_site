cd89ccf0818e48cab4c81e4c42d0e6a4fa1b7386
mami0709 <mmmxxxzelda@yahoo.co.jp>
2022-11-13 17:48:50 +0900

    カート追加

--- layout/Tab.tsx
+++ layout/Tab.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { Box, Text, Image, } from '@chakra-ui/react'
+import { Box, Text, Image, Icon, Button, } from '@chakra-ui/react'
 import  HamburgerMenu  from "./hamburgerMenu"
 import React from 'react';
 import Link from "next/link";
@@ -21,7 +21,14 @@ export function Tab() {
           <Link href='/'> 
             <Text fontSize='4xl' fontFamily="HiraMinProN-W3">リュック専門店</Text>
           </Link>
-          <AiOutlineShoppingCart />
+          <Box marginRight={0} marginLeft={'auto'} marginTop={2} >
+            <Link href='/recomend/inCart' >
+              <Icon w={10} h={10} >
+                <AiOutlineShoppingCart />
+              </Icon>
+            </Link>
+          </Box>
+
           <HamburgerMenu />
           
         </div>
