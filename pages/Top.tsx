ada30c6b79934e44049b15f6999ae51a9ac8b2f7
mami0709 <mmmxxxzelda@yahoo.co.jp>
2022-11-15 15:46:47 +0900

    keyの設定

--- pages/Top.tsx
+++ pages/Top.tsx
@@ -19,9 +19,9 @@ const Top: NextPage = () => {
       <Tab />
       
       <Center paddingTop={"100px"}  width="100%">
-          {taskList.map((product:any, index: React.Key) => (
+          {taskList.map((product:any) => (
             // eslint-disable-next-line react/jsx-key
-            <Box key={index}>
+            <Box >
             <Grid templateColumns='repeat(1, 1fr )' gap={4} >
               <GridItem w='100%' h='10'>
                 <Link href={`/recomend/${product.id}`} >
